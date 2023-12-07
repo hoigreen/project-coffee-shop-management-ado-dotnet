6c03c22fb2ce1593143c824e1921fff245caae9c
dangquochuy <quochuy22122002@gmail.com>
2023-12-07 16:34:06 +0700

    Add class Category

---
+++ BaoCaoCuoiKy/XL_CATEGORY.cs
@@ -0,0 +1,35 @@
+﻿using BaoCaoCuoiKy;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopManagement
+{
+    internal class XL_CATEGORY
+    {
+        private SqlConnection connection;
+        private SqlDataAdapter adapter;
+        private DataSet dataSet;
+        private SqlCommand command;
+        private string connectionString;
+        private Global global = new Global();
+
+        public XL_CATEGORY()
+        {
+            connectionString = global.pathDatabase;
+            connection = new SqlConnection(connectionString);
+        }
+        public DataTable getListNameCategory()
+        {
+            string query = "SELECT * From DANHMUC";
+            adapter = new SqlDataAdapter(query, connection);
+            dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            return dataSet.Tables[0];
+        }
+    }
+}
