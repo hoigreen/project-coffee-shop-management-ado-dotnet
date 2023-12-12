df592a68d3c450008aed4991f4f2a9b81c5a1f8f
HoiGreen <hoitrnd107@gmail.com>
2023-12-12 14:32:49 +0700

    Complete UI of Form_Begin

--- BaoCaoCuoiKy/Form_Begin.Designer.cs
+++ BaoCaoCuoiKy/Form_Begin.Designer.cs
@@ -45,16 +45,17 @@
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Lucida Calligraphy", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblTitle.Location = new System.Drawing.Point(779, 46);
+            this.lblTitle.Location = new System.Drawing.Point(653, 42);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(442, 48);
+            this.lblTitle.Size = new System.Drawing.Size(643, 48);
             this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Quản lý quán cà phê";
+            this.lblTitle.Text = "Quản lý quán cà phê Tình Bạn";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.picLogo);
             this.panel1.Controls.Add(this.lblTitle);
             this.panel1.Location = new System.Drawing.Point(0, 1);
@@ -78,11 +79,11 @@
             this.lblQuestion.AutoSize = true;
             this.lblQuestion.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.lblQuestion.ForeColor = System.Drawing.Color.Green;
-            this.lblQuestion.Location = new System.Drawing.Point(909, 295);
+            this.lblQuestion.Location = new System.Drawing.Point(906, 295);
             this.lblQuestion.Name = "lblQuestion";
-            this.lblQuestion.Size = new System.Drawing.Size(169, 25);
+            this.lblQuestion.Size = new System.Drawing.Size(184, 25);
             this.lblQuestion.TabIndex = 1;
-            this.lblQuestion.Text = "Bạn có vai trò là:";
+            this.lblQuestion.Text = "VAI TRÒ CỦA BẠN";
             this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnExitWindow
@@ -90,22 +91,21 @@
             this.btnExitWindow.Animated = true;
             this.btnExitWindow.BackColor = System.Drawing.Color.Transparent;
             this.btnExitWindow.BorderColor = System.Drawing.Color.IndianRed;
-            this.btnExitWindow.BorderRadius = 20;
-            this.btnExitWindow.BorderThickness = 2;
+            this.btnExitWindow.BorderRadius = 12;
+            this.btnExitWindow.BorderThickness = 1;
             this.btnExitWindow.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnExitWindow.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnExitWindow.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.btnExitWindow.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btnExitWindow.FillColor = System.Drawing.Color.Red;
-            this.btnExitWindow.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitWindow.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExitWindow.ForeColor = System.Drawing.Color.White;
-            this.btnExitWindow.Image = ((System.Drawing.Image)(resources.GetObject("btnExitWindow.Image")));
             this.btnExitWindow.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnExitWindow.ImageSize = new System.Drawing.Size(40, 40);
             this.btnExitWindow.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
-            this.btnExitWindow.Location = new System.Drawing.Point(1647, 999);
+            this.btnExitWindow.Location = new System.Drawing.Point(1716, 1019);
             this.btnExitWindow.Name = "btnExitWindow";
-            this.btnExitWindow.Size = new System.Drawing.Size(261, 69);
+            this.btnExitWindow.Size = new System.Drawing.Size(192, 49);
             this.btnExitWindow.TabIndex = 10;
             this.btnExitWindow.Text = "Thoát hệ thống";
             this.btnExitWindow.Click += new System.EventHandler(this.btnExitWindow_Click);
@@ -123,6 +123,8 @@
             this.btnViewStaff.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(131)))), ((int)(((byte)(248)))));
             this.btnViewStaff.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnViewStaff.ForeColor = System.Drawing.Color.White;
+            this.btnViewStaff.Image = ((System.Drawing.Image)(resources.GetObject("btnViewStaff.Image")));
+            this.btnViewStaff.ImageSize = new System.Drawing.Size(140, 140);
             this.btnViewStaff.Location = new System.Drawing.Point(508, 389);
             this.btnViewStaff.Name = "btnViewStaff";
             this.btnViewStaff.Size = new System.Drawing.Size(423, 343);
@@ -143,6 +145,9 @@
             this.btnViewAdmin.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(159)))), ((int)(((byte)(110)))));
             this.btnViewAdmin.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnViewAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnViewAdmin.Image = ((System.Drawing.Image)(resources.GetObject("btnViewAdmin.Image")));
+            this.btnViewAdmin.ImageSize = new System.Drawing.Size(140, 140);
+            this.btnViewAdmin.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.btnViewAdmin.Location = new System.Drawing.Point(1061, 389);
             this.btnViewAdmin.Name = "btnViewAdmin";
             this.btnViewAdmin.Size = new System.Drawing.Size(423, 343);
@@ -154,7 +159,7 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(245)))), ((int)(((byte)(249)))));
-            this.ClientSize = new System.Drawing.Size(1920, 1061);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.btnViewAdmin);
             this.Controls.Add(this.btnViewStaff);
             this.Controls.Add(this.btnExitWindow);
